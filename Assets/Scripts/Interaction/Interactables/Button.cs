deeaeb828bc6c2dc2eb5927bd81e2029025e60de
Jakub Pawłowicz <126962261+Kubbss@users.noreply.github.com>
2026-02-12 18:21:13 +0000

    Added modular interaction with Unity GameEvents
    Added modular interaction with Unity GameEvents

--- Assets/Scripts/Interaction/Interactables/Button.cs
+++ Assets/Scripts/Interaction/Interactables/Button.cs
@@ -4,10 +4,16 @@ public class Button : Interactable
 {
     [SerializeField]
     private GameObject door;
+    private Animator doorAnimator;
     private bool doorOpen;
+
+    void Start()
+    {
+        doorAnimator = door.GetComponent<Animator>();
+    }
     protected override void Interact()
     {
         doorOpen = !doorOpen;
-        door.GetComponent<Animator>().SetBool("IsOpen", doorOpen);
+        doorAnimator.SetBool("IsOpen", doorOpen);
     }
 }
